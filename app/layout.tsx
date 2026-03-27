51d47cb686d20e61027e376724cab2db189d9842
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2026-03-27 10:55:11 +0200

    define color palette

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en" className={`${inter.variable} h-full antialiased`}>
-			<body className="min-h-full">{children}</body>
+			<body className="bg-background text-background-foreground min-h-full">
+				{children}
+			</body>
 		</html>
 	);
 }
