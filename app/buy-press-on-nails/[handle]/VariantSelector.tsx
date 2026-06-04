dce14ea3a26e32e66185366896820065f6ac9add
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2026-06-04 13:52:38 +0300

    add labels for variant selection dropdowns

--- app/buy-press-on-nails/[handle]/VariantSelector.tsx
+++ app/buy-press-on-nails/[handle]/VariantSelector.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import {AddToCartButton} from '@/components';
-import {SelectField} from '@/components/form';
 import {Dropdown, DropdownItem, Typography} from '@/components/ui';
 import {ShopifyProductOption, ShopifyVariant} from '@/lib/shopify/types';
 import {useState} from 'react';
@@ -30,35 +29,43 @@ const VariantSelector = ({options, variants}: Props) => {
 			{visibleOptions.length !== 0 ? (
 				<div className="flex flex-col gap-4 mb-7">
 					{visibleOptions.map(o => (
-						<Dropdown
-							key={o.name}
-							className="w-full max-w-85"
-							trigger={
-								<button className="flex justify-between items-center gap-1.5 w-full px-4 py-3 border border-border rounded-xl bg-background">
-									<Typography as="span" textTransform="capitalize">
-										{selectedOptions[o.name]}
-									</Typography>
-									<ChevronDown size={16} />
-								</button>
-							}
-							value={selectedOptions[o.name]}
-						>
-							{o.values.map(v => (
-								<DropdownItem
-									key={v}
-									buttonClassName="px-4"
-									onSelect={value => {
-										setSelectedOptions(prev => ({
-											...prev,
-											[o.name]: value,
-										}));
-									}}
-									value={v}
-								>
-									{v}
-								</DropdownItem>
-							))}
-						</Dropdown>
+						<div key={o.name} className="flex flex-col gap-2">
+							<Typography
+								as="label"
+								variant="body2"
+								textTransform="uppercase"
+							>
+								{o.name}
+							</Typography>
+							<Dropdown
+								className="w-full max-w-85"
+								trigger={
+									<button className="flex justify-between items-center gap-1.5 w-full px-4 py-3 border border-border rounded-xl bg-background">
+										<Typography as="span" textTransform="capitalize">
+											{selectedOptions[o.name]}
+										</Typography>
+										<ChevronDown size={16} />
+									</button>
+								}
+								value={selectedOptions[o.name]}
+							>
+								{o.values.map(v => (
+									<DropdownItem
+										key={v}
+										buttonClassName="px-4"
+										onSelect={value => {
+											setSelectedOptions(prev => ({
+												...prev,
+												[o.name]: value,
+											}));
+										}}
+										value={v}
+									>
+										{v}
+									</DropdownItem>
+								))}
+							</Dropdown>
+						</div>
 					))}
 				</div>
 			) : null}
