f061ec09daefe60219193c84a4b6be9e324872ac
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2026-06-04 14:06:12 +0300

    add shadow to header after scroll

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,10 +1,27 @@
+'use client';
+
+import {useState, useEffect} from 'react';
 import LangDropdown from './LangDropdown';
 import Navbar from './Navbar';
 import ShoppingBag from './ShoppingBag';
+import {twMerge} from 'tailwind-merge';
 
-const Header = async () => {
+const Header = () => {
+	const [isScrolled, setIsScrolled] = useState(false);
+
+	useEffect(() => {
+		const handleScroll = () => setIsScrolled(window.scrollY > 0);
+		window.addEventListener('scroll', handleScroll);
+		return () => window.removeEventListener('scroll', handleScroll);
+	}, []);
+
 	return (
-		<header className="sticky top-0 z-30 shadow-border bg-background max-md:py-4">
+		<header
+			className={twMerge(
+				'sticky top-0 z-30 shadow-border bg-background transition-shadow max-md:py-4',
+				isScrolled && 'shadow-md shadow-background-foreground/10',
+			)}
+		>
 			<div className="container container-lg">
 				<div className="grid grid-cols-[1fr_auto_1fr] items-center gap-4 max-lg:grid-cols-[1fr_auto]">
 					<div className="max-lg:hidden" />
