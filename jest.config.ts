bcf67db7d43fc35789e2eb5b023206e85496c36d
Max Kyrychenko <iam.maxkyrychenko@gmail.com>
2026-06-05 14:43:18 +0300

    set up jest

---
+++ jest.config.ts
@@ -0,0 +1,21 @@
+/**
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+import type {Config} from 'jest';
+import nextJest from 'next/jest.js';
+
+const createJestConfig = nextJest({
+	// Provide the path to your Next.js app to load next.config.js and .env files in your test environment
+	dir: './',
+});
+
+// Add any custom config to be passed to Jest
+const config: Config = {
+	clearMocks: true,
+	coverageProvider: 'v8',
+	testEnvironment: 'node',
+};
+
+export default createJestConfig(config);
